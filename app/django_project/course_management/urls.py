e2afd552020472e0038b2fafa24077bf971a467f
Ulises Munñoz <koltis52@gmail.com>
2023-11-30 18:06:23 -0300

    Bien

--- app/django_project/course_management/urls.py
+++ app/django_project/course_management/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('registro/', views.registro_usuario),
+    path('dashboard/', views.dashboard),
     path('perfil_index', views.perfil_index),
     path('proveedores_index', views.proveedores_index),
     path('', views.index, name="index"),
--- app/django_project/course_management/views.py
+++ app/django_project/course_management/views.py
@@ -12,3 +12,28 @@ def perfil_index(request):
 
 def proveedores_index(request):
     return render(request, 'proveedores_index.html')
+
+def dashboard(request):
+    return render(request,"Dashboard.html")
+
+def registro_usuario(request):
+    if request.method == 'POST':
+        run = request.POST.get('run')
+        correo = request.POST.get('correo')
+        nombre = request.POST.get('nombre')
+        tipo_usuario = request.POST.get('tipo_usuario')
+
+        # Crea una instancia del modelo y guarda los datos en la base de datos
+        nuevo_usuario = usuario(run=run, correo=correo, nombre=nombre, tipo=tipo_usuario)
+        nuevo_usuario.save()
+
+        nombre_usuario_registrado = nuevo_usuario.nombre
+
+         # Después de guardar el usuario, registra la acción en la segunda tabla
+        registroSave = registro(Accion=f"Registro de {nombre_usuario_registrado}", id_usuario="Invitado", fecha=datetime.now())
+        registroSave.save()
+
+        # Puedes redirigir a una página de éxito o a donde prefieras
+        return redirect('index')
+
+    return render(request, 'registro.html')
