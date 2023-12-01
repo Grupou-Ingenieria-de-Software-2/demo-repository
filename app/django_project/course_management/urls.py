fe13a7b7c6affca344c068a76ed9274160fb45ab
Ulises Munñoz <koltis52@gmail.com>
2023-12-01 02:37:46 -0300

    Bien

--- app/django_project/course_management/urls.py
+++ app/django_project/course_management/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('dashboard/', views.dashboard),
     path('perfil_index', views.perfil_index),
-    path('proveedores_index', views.proveedores_index),
+    path('proveedores_index', views.provedores),
+    path('perfil_proveedor/<id_usuario>/perfil', views.perfil_proveedor,name='perfil_proveedor'),
     path('', views.index, name="index"),
 ]
--- app/django_project/course_management/views.py
+++ app/django_project/course_management/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from .models import TablaUsuario
 from datetime import datetime
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
     if request.method == 'POST':
@@ -20,8 +21,19 @@ def index(request):
 def perfil_index(request):
     return render(request, 'perfil_index.html')
 
-def proveedores_index(request):
-    return render(request, 'proveedores_index.html')
+#def proveedores_index(request):
+    #return render(request, 'proveedores_index.html')
 
 def dashboard(request):
     return render(request,"Dashboard.html")
+
+def provedores(request):
+    datos_proveedores = TablaUsuario.objects.all()
+    return render(request,"proveedores_index.html", {'datos_usuarios': datos_proveedores})
+#-----------------------------------------------
+def perfil_proveedor(request, id_usuario):
+    # Obtén el proveedor específico o muestra un error 404 si no se encuentra
+    proveedor = get_object_or_404(TablaUsuario, id_usuario=id_usuario)
+
+    # Pasa los detalles del proveedor a la plantilla
+    return render(request, 'perfil_proveedor.html', {'proveedor': proveedor})
